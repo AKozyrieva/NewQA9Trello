d158be03decbf3b1511c5a95af3ce1d2f76bb12b
AKozyrieva <a.kurkova@ukr.net>
2021-06-26 21:17:06 +0300

    last homework

--- src/com/company/test/CurrentBoardTests.java
+++ src/com/company/test/CurrentBoardTests.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CurrentBoardTests extends TestBase {
     @BeforeMethod
     public void initTests() throws InterruptedException {
@@ -74,4 +76,85 @@ public class CurrentBoardTests extends TestBase {
         field.click();
         field.sendKeys(value);
     }
-}
+
+    @Test
+    public void archiveList() throws InterruptedException {
+        List<WebElement> existList = driver.findElements(By.cssSelector(".js-editing-target"));
+        if (existList.size() > 0) {
+            WebElement buttonAction = driver.findElement(By.cssSelector(".list-header-extras"));
+            buttonAction.click();
+            Thread.sleep(1000);
+            WebElement archiveList = driver.findElement(By.cssSelector(".js-close-list"));
+            archiveList.click();
+            Thread.sleep(5000);
+        } else {
+            WebElement createList = driver.findElement(By.cssSelector(".placeholder"));
+            createList.click();
+            Thread.sleep(1000);
+
+            WebElement listTitle = driver.findElement(By.cssSelector("input[name='name']"));
+            editField(listTitle, "New");
+            Thread.sleep(3000);
+
+            WebElement saveNewList = driver.findElement(By.cssSelector(".js-save-edit"));
+            saveNewList.click();
+            Thread.sleep(2000L);
+
+            WebElement buttonAction = driver.findElement(By.cssSelector(".list-header-extras"));
+            buttonAction.click();
+            Thread.sleep(3000);
+            WebElement archiveList = driver.findElement(By.cssSelector(".js-close-list"));
+            archiveList.click();
+            Thread.sleep(3000);
+
+
+        }
+    }
+
+        @Test
+        public void copyList() throws InterruptedException {
+            List<WebElement> existList1 = driver.findElements(By.cssSelector(".js-editing-target"));
+            if (existList1.size() > 0) {
+                WebElement buttonAction = driver.findElement(By.cssSelector(".list-header-extras"));
+                buttonAction.click();
+                Thread.sleep(1000);
+                WebElement copyList = driver.findElement(By.cssSelector(".js-copy-list"));
+                copyList.click();
+                Thread.sleep(1000);
+                WebElement copyButton = driver.findElement(By.cssSelector(".js-submit"));
+                copyButton.click();
+                Thread.sleep(5000);
+
+            } else {
+                WebElement createList = driver.findElement(By.cssSelector(".placeholder"));
+                createList.click();
+                Thread.sleep(1000);
+
+                WebElement listTitle = driver.findElement(By.cssSelector("input[name='name']"));
+                editField(listTitle, "New");
+                Thread.sleep(3000);
+
+                WebElement saveNewList = driver.findElement(By.cssSelector(".js-save-edit"));
+                saveNewList.click();
+                Thread.sleep(2000L);
+
+                WebElement buttonAction = driver.findElement(By.cssSelector(".list-header-extras"));
+                buttonAction.click();
+                Thread.sleep(1000);
+                WebElement copyList = driver.findElement(By.cssSelector(".js-copy-list"));
+                copyList.click();
+                Thread.sleep(1000);
+                WebElement copyButton = driver.findElement(By.cssSelector(".js-submit"));
+                copyButton.click();
+                Thread.sleep(5000);
+
+            }}
+            }
+
+
+
+
+
+
+
+
